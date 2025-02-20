451b6ae685f23b660509444fc1e639103ad5c90a
A-Slimani <abdullahslimani98@gmail.com>
2025-02-20 15:15:37 +1100

    updates

--- UFCScraper/UFCScraper/pipelines.py
+++ UFCScraper/UFCScraper/pipelines.py
@@ -1,10 +1,9 @@
-from dotenv import load_dotenv
 from UFCScraper.items import EventItem, FighterItem
+from dotenv import load_dotenv 
 import psycopg2
 import os
 
 load_dotenv()
-
 
 class EventPipeline:
     def __init__(self):
@@ -59,8 +58,6 @@ class EventPipeline:
 
             conn.close()
             return item
-        else:
-            print(f"Invalid item :: {type(item)}")
 
 
 class FighterPipeline:
@@ -96,99 +93,100 @@ class FighterPipeline:
         conn.close()
 
     def process_item(self, item, spider):
-        conn = psycopg2.connect(os.getenv("DB_URI"))
-        cursor = conn.cursor()
-        cursor.execute("SELECT url FROM fighters WHERE url=%s", (item["url"],))
-        existing_event = cursor.fetchone()
+        if isinstance(item, FighterItem):
+            conn = psycopg2.connect(os.getenv("DB_URI"))
+            cursor = conn.cursor()
+            cursor.execute("SELECT url FROM fighters WHERE url=%s", (item["url"],))
+            existing_event = cursor.fetchone()
 
-        if existing_event:
-            # update the event
-            update_query = """
-                UPDATE fighters 
-                SET 
-                    name = %s,
-                    nickname = %s,
-                    status = %s,
-                    record_raw = %s,
-                    wins = %s,
-                    losses = %s,
-                    draws = %s,
-                    weight_class = %s,
-                    age = %s,
-                    height = %s,
-                    reach = %s,
-                    leg_reach = %s,
-                    hometown = %s,
-                    trains_at = %s,
-                    fighting_style = %s,
-                    last_updated_at = CURRENT_TIMESTAMP 
-                WHERE url = %s
-            """
-            cursor.execute(
-                update_query,
-                (
-                    item["name"],
-                    item["nickname"],
-                    item["status"],
-                    item["record_raw"],
-                    item["wins"],
-                    item["losses"],
-                    item["draws"],
-                    item["weight_class"],
-                    item["age"],
-                    item["height"],
-                    item["reach"],
-                    item["leg_reach"],
-                    item["hometown"],
-                    item["trains_at"],
-                    item["fighting_style"],
-                ),
-            )
-            conn.commit()
+            if existing_event:
+                # update the event
+                update_query = """
+                    UPDATE fighters 
+                    SET 
+                        name = %s,
+                        nickname = %s,
+                        status = %s,
+                        record_raw = %s,
+                        wins = %s,
+                        losses = %s,
+                        draws = %s,
+                        weight_class = %s,
+                        age = %s,
+                        height = %s,
+                        reach = %s,
+                        leg_reach = %s,
+                        hometown = %s,
+                        trains_at = %s,
+                        fighting_style = %s,
+                        last_updated_at = CURRENT_TIMESTAMP 
+                    WHERE url = %s
+                """
+                cursor.execute(
+                    update_query,
+                    (
+                        item["name"],
+                        item["nickname"],
+                        item["status"],
+                        item["record_raw"],
+                        item["wins"],
+                        item["losses"],
+                        item["draws"],
+                        item["weight_class"],
+                        item["age"],
+                        item["height"],
+                        item["reach"],
+                        item["leg_reach"],
+                        item["hometown"],
+                        item["trains_at"],
+                        item["fighting_style"],
+                    ),
+                )
+                conn.commit()
 
-        else:
-            insert_query = """
-                INSERT INTO fighters 
-                (
-                    name,
-                    nickname,
-                    status,
-                    record_raw,
-                    wins,
-                    losses,
-                    draws,
-                    weight_class,
-                    age,
-                    height,
-                    reach,
-                    leg_reach,
-                    hometown,
-                    trains_at,
-                    fighting_style,
-                    last_updated_at
-                ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) 
-            """
-            cursor.execute(
-                insert_query,
-                (
-                    item["name"],
-                    item["nickname"],
-                    item["status"],
-                    item["record_raw"],
-                    item["wins"],
-                    item["losses"],
-                    item["draws"],
-                    item["weight_class"],
-                    item["age"],
-                    item["height"],
-                    item["reach"],
-                    item["leg_reach"],
-                    item["hometown"],
-                    item["trains_at"],
-                    item["fighting_style"],
-                ),
-            )
-            conn.commit()
+            else:
+                insert_query = """
+                    INSERT INTO fighters 
+                    (
+                        name,
+                        nickname,
+                        status,
+                        record_raw,
+                        wins,
+                        losses,
+                        draws,
+                        weight_class,
+                        age,
+                        height,
+                        reach,
+                        leg_reach,
+                        hometown,
+                        trains_at,
+                        fighting_style,
+                        last_updated_at
+                    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP) 
+                """
+                cursor.execute(
+                    insert_query,
+                    (
+                        item["name"],
+                        item["nickname"],
+                        item["status"],
+                        item["record_raw"],
+                        item["wins"],
+                        item["losses"],
+                        item["draws"],
+                        item["weight_class"],
+                        item["age"],
+                        item["height"],
+                        item["reach"],
+                        item["leg_reach"],
+                        item["hometown"],
+                        item["trains_at"],
+                        item["fighting_style"],
+                    ),
+                )
+                conn.commit()
 
-        conn.close()
-        return item
+            conn.close()
+            return item
--- UFCScraper/UFCScraper/spiders/inital_build.py
+++ UFCScraper/UFCScraper/spiders/inital_build.py
@@ -1,7 +1,7 @@
 from scrapy.selector.unified import Selector
 from scrapy.exceptions import CloseSpider
 from UFCScraper.items import EventItem, FighterItem
-from UFCScraper.pipelines import EventPipeline, FighterPipeline 
+from UFCScraper.pipelines import EventPipeline, FighterPipeline
 import scrapy
 
 
@@ -12,7 +12,7 @@ class FightersSpider(scrapy.Spider):
 
     def clean_text(self, text) -> str:
         return text.replace("\n", "").strip()
-    
+
     def inches_to_cm(self, value):
         if value != "":
             return round(float(value) * 2.54)
@@ -27,12 +27,12 @@ class FightersSpider(scrapy.Spider):
             raise CloseSpider("No more pages to scrape")
 
         ## UNCOMMENT ONCE DONE TESTING
-        # for url in urls:
-        #     yield response.follow(url=url, callback=self.parse_urls)
+        for url in urls:
+            yield response.follow(url=url, callback=self.parse_urls)
 
         # USE ONLY FOR TESTING
-        for x in range(2):
-            yield response.follow(url=urls[x], callback=self.parse_urls)
+        # for x in range(2):
+        #     yield response.follow(url=urls[x], callback=self.parse_urls)
 
     def parse_urls(self, response):
         selector = Selector(text=response.text)
@@ -47,8 +47,8 @@ class FightersSpider(scrapy.Spider):
                 yield response.follow(url=url, callback=self.parse_athletes)
 
             ## UNCOMMENT ONCE DONE TESTING
-            # if "/event/" in url:
-            #     yield response.follow(url=url, callback=self.parse_events)
+            if "/event/" in url:
+                yield response.follow(url=url, callback=self.parse_events)
 
     def parse_athletes(self, response):
         selector = Selector(text=response.text)
@@ -58,7 +58,9 @@ class FightersSpider(scrapy.Spider):
         record_raw = selector.xpath(
             "//p[@class='hero-profile__division-body']/text()"
         ).get()
-        record_processed = record_raw.split(" ")[0].split("-")
+        record_processed = (
+            record_raw.split(" ")[0].split("-") if record_raw else [None, None, None]
+        )
         wins = record_processed[0]
         losses = record_processed[1]
         draws = record_processed[2]
@@ -66,7 +68,11 @@ class FightersSpider(scrapy.Spider):
         weight_class_raw = selector.xpath(
             "//p[@class='hero-profile__division-title']/text()"
         ).get()
-        weight_class_cleaned = weight_class_raw.replace(" Division", "").strip()
+        weight_class_cleaned = (
+            weight_class_raw.replace(" Division", "").strip()
+            if weight_class_raw
+            else None
+        )
 
         bio_field = selector.xpath("//div[@class='c-bio__field']")
         paired_bio = []
@@ -80,14 +86,14 @@ class FightersSpider(scrapy.Spider):
 
         hometown = None
         try:
-            if "Status" in bio_details: 
+            if "Status" in bio_details:
                 hometown_element = selector.xpath(
                     "//div[@class='c-bio__row--1col']/div/div[@class='c-bio__text']/text()"
                 ).getall()
                 if len(hometown_element) > 1:
                     hometown = hometown_element[1]
                 else:
-                    hometown = hometown_element[0] 
+                    hometown = hometown_element[0]
 
             else:
                 hometown = selector.xpath(
@@ -96,38 +102,44 @@ class FightersSpider(scrapy.Spider):
         except:
             print(f"Missing hometown data for: {name}")
 
-        row_2col = []
-        try:
-            row_2col = selector.xpath(
-                "//div[@class='c-bio__row--2col']/div/div[@class='c-bio__text']/text()"
-            ).getall()
-            if len(row_2col) == 1:
-                row_2col.append(None)
-            elif len(row_2col) == 0:
-                row_2col = [None, None]
-        except:
-            print(f"Missing trains_at and or fighting_style data for: {name}")
+        row_2col = selector.xpath("//div[@class='c-bio__row--2col']")
+        values = []
+        fields = row_2col.xpath("//div[@class='c-bio__row--2col']/div[@class='c-bio__field c-bio__field--border-bottom-small-screens']")
+        for field in fields:
+            label = field.xpath("div[@class='c-bio__label']/text()").get()
+            text = field.xpath("div[@class='c-bio__text']/text()").get()
+            values.append((label, text))
+        values_dict = dict(values)
 
         fighter = FighterItem()
         fighter["name"] = name
         fighter["nickname"] = nickname
-        fighter["status"] = bio_details["Status"] if "Status" in bio_details else None 
+        fighter["status"] = bio_details["Status"] if "Status" in bio_details else None
         fighter["record_raw"] = record_raw
         fighter["wins"] = wins
         fighter["losses"] = losses
         fighter["draws"] = draws
         fighter["weight_class"] = weight_class_cleaned
-        fighter["age"] = bio_details["Age"] if "Age" in bio_details else None 
-        fighter["height"] = self.inches_to_cm(bio_details["Height"]) if "Height" in bio_details else None
-        fighter["reach"] = self.inches_to_cm(bio_details["Reach"]) if "Reach" in bio_details else None
-        fighter["leg_reach"] = self.inches_to_cm(bio_details["Leg reach"]) if "Leg reach" in bio_details else None
+        fighter["age"] = bio_details["Age"] if "Age" in bio_details else None
+        fighter["height"] = (
+            self.inches_to_cm(bio_details["Height"])
+            if "Height" in bio_details
+            else None
+        )
+        fighter["reach"] = (
+            self.inches_to_cm(bio_details["Reach"]) if "Reach" in bio_details else None
+        )
+        fighter["leg_reach"] = (
+            self.inches_to_cm(bio_details["Leg reach"])
+            if "Leg reach" in bio_details
+            else None
+        )
         fighter["hometown"] = hometown
-        fighter["trains_at"] = row_2col[0]
-        fighter["fighting_style"] = row_2col[1]
+        fighter["trains_at"] = values_dict["Trains at"] if "Trains at" in values_dict else None
+        fighter["fighting_style"] = values_dict["Fighting style"] if "Fighting style" in values_dict else None
         fighter["url"] = response.url
 
-        yield FighterPipeline().process_item(fighter, '')
-
+        yield FighterPipeline().process_item(fighter, "")
 
     def parse_events(self, response):
         selector = Selector(text=response.text)
@@ -180,4 +192,4 @@ class FightersSpider(scrapy.Spider):
         event["date_raw"] = date_raw
         event["url"] = response.url
 
-        yield event
+        yield EventPipeline().process_item(event, "")
